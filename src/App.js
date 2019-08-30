7c2d36931af7f9b93c7a6c94de4d6de974242349
btan <btan7645@gmail.com>
2019-08-30 13:01:11 -0400

    finished form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import 'tachyons';
 
 function App() {
